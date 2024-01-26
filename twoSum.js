9b23f6c7d6f3c8a846d63128000deb8c0e0c7567
Mohammed Tajammul <76564671+Tajammul607@users.noreply.github.com>
2024-01-26 15:28:15 +0000

    solved using brute force and hashing

---
+++ twoSum.js
@@ -0,0 +1,63 @@
+// in this approach we're checking each pair of the ele 
+//the time complextiy is gona be O(n^2)
+
+const twoSumBruteForce= (num,target)=>{
+    for(let i=0; i<num.length; i++){
+        for(let j=i+1; j<num.length-1; j++){
+            if(num[i]+num[j]==target){
+                return [i,j]
+            }
+        }
+    }
+    return -1
+}
+
+let arr=[2,1,2,3,4,5]
+let target=5;
+
+console.log(twoSumBruteForce(arr,target))
+
+const twoSumUsingHash=(nums,target)=>{
+   
+    let usingMap=new Map();
+
+    for(let i=0; i<nums.length; i++){
+        let diff=target-nums[i]
+
+        if(usingMap.has(diff)){
+            return [usingMap.get(diff),i] // if exist get the element along with index
+        }
+
+        usingMap.set(nums[i],i) // set the element and set the index in the hash
+    }
+
+    return[]
+}
+
+console.log(twoSumUsingHash(arr,target))
+
+
+function reverseAStringByWord(str){
+    let reverseWord= str.split(" ").map(word=>{
+        return word.split('').reverse().join('')
+    })
+
+   return reverseWord
+}
+
+console.log(reverseAStringByWord("This is gonna be amazing"))
+
+const fizzBuzzSequence = (start, end) => {
+    for (let num = start; num <= end; num++) {
+      if (num % 3 === 0 && num % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (num % 3 === 0) {
+        console.log("Fizz");
+      } else if (num % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(num);
+      }
+    }
+  };
+console.log(fizzBuzzSequence(1,10))
